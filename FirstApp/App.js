28aef9fbf054155796dad6112c473a164b251b8c
Khaldoun Halawani <kldoon@hebron.edu>
2026-02-16 09:14:37 +0200

    [16-2] Components

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// const App=()=>
+const Welcome = () =>{
+  return(
+    <View>
+      <Text>==================</Text>
+      <Text>Welcome to the APP</Text>
+      <Text>==================</Text>
+    </View>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Welcome />
       <Text>Welcome to my First App! Hello</Text>
+      <Welcome />
       <StatusBar style="auto" />
     </View>
   );
