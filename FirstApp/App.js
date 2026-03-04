822c0367c9b6c02d5bab223cb19b825d440c3b9c
Khaldoun Halawani <khalawani@sadasol.com>
2026-03-04 09:56:47 +0200

    [4-3] Rendering Lists

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import StudentCard from './components/StudentCard';
 import Divider from './components/Divider';
 import { useState } from 'react';
 
 export default function App() {
   //New student Info
-  const [stdName, setStdName] = useState("");
+  const [stdName, setStdName] = useState();
   const [stdId, setStdId] = useState();
+  const STUDENT_DATA = [
+    {
+      id: 1233,
+      name: "Ahmad Saeed",
+      mark: 90
+    },
+    {
+      id: 1324,
+      name: "Sarah Jameel",
+      mark: 87
+    },
+    {
+      id: 1342,
+      name: "Waleed Nabil",
+      mark: 78
+    },
+    // {
+    //   id: 1343,
+    //   name: "Hiba Nabil",
+    //   mark: 90
+    // },
+  ]
 
   // const handleChangeName = (value) => {
   //   //console.log(value);
@@ -20,20 +42,27 @@ export default function App() {
   }
 
   const clear = () => {
-
+    setStdName("");
+    setStdId("");
   }
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} onChangeText={handleIdChange} placeholder="Student Id" />
-      <TextInput style={styles.input} onChangeText={(value) => setStdName(value)} placeholder="Student Name" />
+      {/* Controlled components, when you send the value to the inputs */}
+      <TextInput style={styles.input} inputMode="numeric" onChangeText={handleIdChange} placeholder="Student Id" value={stdId} />
+      <TextInput style={styles.input} onChangeText={(value) => setStdName(value)} placeholder="Student Name" value={stdName} />
+      <Button title="Clear" onPress={clear} />
+      <Button title="Submit" onPress={() => { }} />
       <Divider />
       <Text>New Student ID: {stdId}</Text>
       <Text>New Student Name: {stdName}</Text>
       <Divider />
-      <StudentCard studentId={1233} studentName="Ahmad Saeed" mark={90} />
-      <StudentCard studentId={1324} studentName="Sarah Jameel" mark="87" />
-      <StudentCard studentId={1342} studentName="Waleed Nabil" mark={78} />
+      {
+        // List rendering in react
+        STUDENT_DATA.map((student) => {
+          return <StudentCard key={student.id} studentId={student.id} studentName={student.name} mark={student.mark} />
+        })
+      }
       <Divider />
       <Text>Welcome to my First App! Hello</Text>
       <Divider />
@@ -53,7 +82,7 @@ const styles = StyleSheet.create({
     borderColor: "#4455f2",
     borderWidth: 1,
     borderRadius: 5,
-    paddingHorizontal: 10,
+    paddingHorizontal: 2,
     width: "50%",
     marginBottom: 10
   }
